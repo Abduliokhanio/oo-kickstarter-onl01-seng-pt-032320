0ffd79daf7c74bb7c0355d2e067b762ed6a0a8b4
Abdul Khan <akhan17@outlook.com>
2020-04-16 22:10:48 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,23 +1,19 @@
-class Backer 
-  
+
+class Backer
+
   attr_reader :name
-<<<<<<< HEAD
+
   attr_accessor :backed_projects
-  
+
   def initialize(name)
     @name = name
     @backed_projects = []
-  end 
-  
+  end
+
   def back_project(project)
     @backed_projects << project
 
     project.backers << self
   end
-=======
-  
-  def initialize(name)
-    @name = name
-  end 
->>>>>>> 308f5017fe0126b3ed214f9d1f64d23a168d44ac
+
 end
